519ee112be55de98bf5a768604ffb940ba445216
HanHoney <43615216@qq.com>
2018-12-11 21:28:09 +0800

    Update 128.cpp

--- 128.cpp
+++ 128.cpp
@@ -1,7 +1,8 @@
 //128. Longest Consecutive Sequence 
 //最长连续子序列
 //Hard
-//①sort	②HashSet(空间换时间)
+//①sort	②哈希表(空间换时间)
+
 /****************************
 Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
 Your algorithm should run in O(n) complexity.
@@ -17,12 +18,13 @@ Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefor
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 class Solution {
 public:
 
-	//① TC:O(nlogn)	SC:O(n)
+	//① TC:O(nlogn)	SC:O(1)
 	int longestConsecutive(vector<int>& nums) {
 		if (nums.empty()) return 0;
 		sort(nums.begin(), nums.end());
@@ -46,7 +48,7 @@ public:
 
 	//② TC:O(n)	SC:O(n)
 	int longestConsecutive1(vector<int>& nums) {
-		unordered_map<int, int> map;
+		unordered_map<int, int> map;	//基于哈希表
 		int ret = 0;
 		for (int i : nums) {
 			if (map[i] == 0) {
@@ -59,13 +61,36 @@ public:
 		return ret;
 	}
 
+	int longestConsecutive2(vector<int>& nums) {
+		if (nums.size() <= 1) return nums.size();
+		unordered_set<int> s;
+		for (auto i : nums) {
+			s.insert(i);
+		}
+		int ret = 0;
+		for (auto i : nums) {
+			int tmp = 1;
+			for (int j = i - 1; s.find(j) != s.end(); j--) {
+				s.erase(j);
+				tmp++;
+			}
+			for (int j = i + 1; s.find(j) != s.end(); j++) {
+				s.erase(j);
+				tmp++;
+			}
+			ret = max(ret, tmp);
+		}
+		return ret;
+	}
 	
 };
 
 int main() {
 	vector<int> nums{ 9,1,4,7,3,-1,0,5,8,-1,6 };
 	Solution s;
+	cout << s.longestConsecutive(nums) << endl;
 	cout << s.longestConsecutive1(nums) << endl;
+	cout << s.longestConsecutive2(nums) << endl;
 	system("pause");
 	return 0;
 }
