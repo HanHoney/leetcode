c2ba8d1137e4caae59413f552b8b4f0ffe8f11ff
HanHoney <43615216@qq.com>
2018-12-12 21:48:25 +0800

    Add files via upload

---
+++ 283. Move Zeroes.cpp
@@ -0,0 +1,50 @@
+//283. Move Zeroes
+//Easy
+//two pointers
+//TC:O(n)	SC:O(1) in-place
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	void moveZeroes(vector<int>& nums) {
+		for (int i = 0, j = 0; j < nums.size(); j++) {
+			if (nums[j] != 0) {
+				swap(nums[i++], nums[j]);
+			}
+		}
+	}
+	/*
+	void moveZeroes1(vector<int>& nums) {
+		if (nums.empty()) return;
+		int cnt = 0;
+		for (auto n : nums) {
+			if (n == 0) cnt++;
+		}
+		if (cnt == 0) return;
+		int idx = 0;
+		for (auto n : nums) {
+			if (n != 0) {
+				nums[idx++] = n;
+			}
+		}
+		for (int i = idx; i < nums.size(); i++) {
+			nums[i] = 0;
+		}
+	}
+	*/
+};
+
+int main() {
+	Solution s;
+	vector<int> nums{ 0,1,0,3,12 };
+	s.moveZeroes(nums);
+	for (auto n : nums) {
+		cout << n << " ";
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
