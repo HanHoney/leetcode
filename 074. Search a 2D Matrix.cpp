3c7c476277db0615b6f348a5aef67c3a800689f6
HanHoney <43615216@qq.com>
2018-12-16 21:04:17 +0800

    Update 074. Search a 2D Matrix.cpp

--- 074. Search a 2D Matrix.cpp
+++ 074. Search a 2D Matrix.cpp
@@ -11,41 +11,9 @@ class Solution {
 
 public:
 
-	//Á½´ÎÑ­»·£¬·Ö±ðÈ·¶¨ÐÐºÍÁÐ¡£
-	//TC:O(logm+logn)
-
-	bool searchMatrix(vector<vector<int>>& matrix, int target) {
-		if (matrix.empty() || matrix[0].empty()) return false;
-		int m = matrix.size();
-		int n = matrix[0].size();
-		//È·¶¨row
-		int rbegin = 0, rend = m - 1;   //rowBegin
-		int row = rbegin;
-		while (rbegin <= rend) {
-			int rmid = (rbegin + rend) / 2;
-			if (matrix[rmid][0] == target) return true;
-			if (matrix[rmid][0] < target) {
-				row = rmid;
-				rbegin = rmid + 1;
-			}
-			else rend = rmid - 1;
-		}
-		//È·¶¨column
-		int cbegin = 0, cend = n - 1;    //columnBegin
-		while (cbegin <= cend) {
-			int cmid = (cbegin + cend) / 2;
-			if (matrix[row][cmid] == target) return true;
-			if (matrix[row][cmid] < target) {
-				cbegin = cmid + 1;
-			}
-			else cend = cmid - 1;
-		}
-		return false;
-	}
-
-	//´ÓÓÒÉÏ½Ç¿ªÊ¼
+	//ä»Žå³ä¸Šè§’å¼€å§‹
 	//TC:O(m+n)
-	bool searchMatrix1(vector<vector<int>>& matrix, int target) {
+	bool searchMatrix(vector<vector<int>>& matrix, int target) {
 		if (matrix.empty() || matrix[0].empty()) return false;
 		int row = 0, col = matrix[0].size() - 1;
 		while (row < matrix.size() && col >= 0) {
@@ -56,9 +24,9 @@ public:
 		return false;
 	}
 
-	//´Ó×óÏÂ½Ç¿ªÊ¼
+	//ä»Žå·¦ä¸‹è§’å¼€å§‹
 	//TC:O(m+n)
-	bool serachMatrix2(vector<vector<int>>& matrix, int target) {
+	bool serachMatrix1(vector<vector<int>>& matrix, int target) {
 		if (matrix.empty() || matrix[0].empty()) return false;
 		int row = matrix.size() - 1, col = 0;
 		while (row >= 0 && col < matrix[0].size()) {
@@ -70,18 +38,18 @@ public:
 	}
 };
 
-//
-//int main() {
-//	vector<vector<int>> vec1{ {10,12},{15,20},{21,26} };
-//	vector<vector<int>> vec2{ {10,12},{15,20},{23,26} };
-//	vector<vector<int>> vec3{};
-//	vector<vector<int>> vec4{ {} };
-//	int tar = 21;
-//	Solution s;
-//	cout << s.searchMatrix(vec1, tar) << endl;
-//	cout << s.searchMatrix(vec2, tar) << endl;
-//	cout << s.searchMatrix(vec3, tar) << endl;
-//	cout << s.searchMatrix(vec4, tar) << endl;
-//	system("pause");
-//	return 0;
-//}
+
+int main() {
+	vector<vector<int>> vec1{ {10,12},{15,20},{21,26} };
+	vector<vector<int>> vec2{ {10,12},{15,20},{23,26} };
+	vector<vector<int>> vec3{};
+	vector<vector<int>> vec4{ {} };
+	int tar = 21;
+	Solution s;
+	cout << s.searchMatrix(vec1, tar) << endl;
+	cout << s.searchMatrix(vec2, tar) << endl;
+	cout << s.searchMatrix(vec3, tar) << endl;
+	cout << s.searchMatrix(vec4, tar) << endl;
+	system("pause");
+	return 0;
+}
