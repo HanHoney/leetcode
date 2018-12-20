2a018aee5aadde75f430a80594b41d6a65e2aab2
HanHoney <43615216@qq.com>
2018-12-20 21:05:55 +0800

    Add files via upload

---
+++ 300. Longest Increasing Subsequence.cpp
@@ -0,0 +1,41 @@
+//300. Longest Increasing Subsequence
+//最长递增子序列(LIS)
+//Medium
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+
+	//①DP
+	//TC:O(n^2)	SC:O(n)
+	//dp[i]=max(dp[i],dp[j]+1)&&nums[j]<nums[i]
+	//init:dp[i]=1
+	int lengthOfLIS(vector<int>& nums) {
+		if (nums.empty()) return 0;
+		vector<int> dp(nums.size(), 1);
+		int ret = 1;
+		for (int i = 1; i < nums.size(); i++) {
+			for (int j = 0; j < i; j++) {
+				if (nums[j] < nums[i]) {
+					dp[i] = max(dp[i], dp[j] + 1);
+				}
+			}
+			ret = max(ret, dp[i]);
+		}
+		return ret;
+	}
+
+	//②进阶版 TC:O(nlogn)???
+};
+
+int main() {
+	vector<int> nums{ 10,9,2,5,3,7,101,18 };
+	Solution s;
+	cout << s.lengthOfLIS(nums) << endl;
+	system("pause");
+	return 0;
+}
