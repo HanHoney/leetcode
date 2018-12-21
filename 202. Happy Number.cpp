3e2ef5f596e8ca3f6673436cf5cd9fb7ba74691c
HanHoney <43615216@qq.com>
2018-12-21 20:10:17 +0800

    Add files via upload

---
+++ 202. Happy Number.cpp
@@ -0,0 +1,43 @@
+//202. Happy Number
+//Easy
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	bool isHappy(int n) {
+		int curr = n;
+		int next;
+		unordered_set<int> s;
+		s.insert(n);
+		while(true) {
+			next = nextNum(curr);
+			if (next == 1) return true;
+			if (s.find(next) != s.end()) return false;
+			s.insert(next);
+			curr = next;
+		} 
+	}
+private:
+	static int nextNum(int n) {
+		int next = 0;
+		while (n) {
+			next += (n % 10)*(n % 10);
+			n /= 10;
+		}
+		return next;
+	}
+};
+
+int main() {
+	Solution s;
+	int n;
+	while (cin >> n) {
+		cout << s.isHappy(n) << endl;
+	}
+	system("pause");
+	return 0;
+}
