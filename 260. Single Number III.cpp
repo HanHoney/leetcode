b275d70cfff441137c4d43beeb586d2ef18fc89f
HanHoney <43615216@qq.com>
2019-02-22 20:43:00 +0800

    Add files via upload

---
+++ 260. Single Number III.cpp
@@ -0,0 +1,30 @@
+//260. Single Number III
+//Medium
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> singleNumber(vector<int>& nums) {
+		int tmp = 0;
+		for (auto num : nums) {
+			tmp ^= num;
+		}
+		int a = 0x1;
+		while ((tmp&a) == 0) {   //注意优先级，加括号
+			a = a << 1;
+		}
+		vector<int> res({ 0,0 });
+		for (auto num : nums) {
+			if (num&a) {
+				res[0] ^= num;
+			}
+			else {
+				res[1] ^= num;
+			}
+		}
+		return res;
+	}
+};
