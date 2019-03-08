d4b7c6ea5eecf9290abe0b8bd86e74d446f173da
HanHoney <43615216@qq.com>
2019-03-08 23:12:31 +0800

    Add files via upload

---
+++ 189. Rotate Array.cpp
@@ -0,0 +1,25 @@
+﻿//189. Rotate Array
+//Easy
+//rorate => reverse
+// (XY)'=Y'X',(Y')'(X')'=YX
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	void rotate(vector<int>& nums, int k) {
+		if (nums.size() < 2) return;
+		k %= nums.size();
+		if (k == 0) return;
+		reverse(nums, 0, nums.size() - 1);
+		reverse(nums, 0, k - 1);
+		reverse(nums, k, nums.size() - 1);
+	}
+	void reverse(vector<int>& nums, int begin, int end) {
+		while (begin < end) {
+			swap(nums[begin++], nums[end--]);
+		}
+	}
+};
---
+++ 412. Fizz Buzz.cpp
@@ -0,0 +1,26 @@
+﻿//412. Fizz Buzz
+//Easy
+
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	vector<string> fizzBuzz(int n) {
+		vector<string> strs(n, "");
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0) {
+				strs[i - 1] += "Fizz";
+			}
+			if (i % 5 == 0) {
+				strs[i - 1] += "Buzz";
+			}
+			if (i % 3 != 0 && i % 5 != 0) {
+				strs[i - 1] += to_string(i);
+			}
+		}
+		return strs;
+	}
+};
