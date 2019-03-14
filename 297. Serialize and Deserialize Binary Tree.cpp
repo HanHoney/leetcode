eafb72bdf0dc90abc521d7747b007d6cf840e946
HanHoney <43615216@qq.com>
2019-03-14 21:25:56 +0800

    Add files via upload

---
+++ 297. Serialize and Deserialize Binary Tree.cpp
@@ -0,0 +1,43 @@
+﻿//297. Serialize and Deserialize Binary Tree
+//Hard
+//序列化和反序列化二叉树 剑指offer
+//使用先序遍历
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) :val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+	// Encodes a tree to a single string.
+	string serialize(TreeNode* root) {
+		if (root == nullptr) return "#";
+		return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
+	}
+
+	// Decodes your encoded data to tree.
+	TreeNode* deserialize(string data) {
+		return deserializeHelper(data);
+	}
+
+private:
+	TreeNode * deserializeHelper(string& str) {
+		if (str[0] == '#') {
+			if (str.size() > 1) str = str.substr(2);	//默认从开始位置到尾
+			return nullptr;
+		}
+		int pos = str.find_first_of(',');
+		TreeNode* ret = new TreeNode(stoi(str.substr(0, pos)));
+		str = str.substr(pos + 1);
+		ret->left = deserializeHelper(str);
+		ret->right = deserializeHelper(str);
+		return ret;
+	}
+};
---
+++ 771. Jewels and Stones.cpp
@@ -0,0 +1,39 @@
+﻿//771. Jewels and Stones
+//Easy
+//hash table
+
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int numJewelsInStones(string J, string S) {
+		if (J.empty() || S.empty()) return 0;
+		int ret = 0;
+		bool lower[26] = { false };
+		bool upper[26] = { false };
+		for (int i = 0; i < J.size(); i++) {
+			char ch = J[i];
+			if (ch >= 'a'&&ch <= 'z') {
+				lower[ch - 'a'] = true;
+			}
+			else if (ch >= 'A'&&ch <= 'Z') {
+				upper[ch - 'A'] = true;
+			}
+		}
+		for (int i = 0; i < S.size(); i++) {
+			char ch = S[i];
+			if (ch >= 'a'&&ch <= 'z') {
+				if (lower[ch - 'a']) {
+					ret++;
+				}
+			}
+			else if (ch >= 'A'&&ch <= 'Z') {
+				if (upper[ch - 'A']) {
+					ret++;
+				}
+			}
+		}
+		return ret;
+	}
+};
